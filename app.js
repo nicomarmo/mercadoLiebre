0868e8ef447d54efe864efc44c50110b774497bf
nicomarmo <marmonicolas10@gmail.com>
2021-09-20 21:16:24 -0300

    horrible me quedo

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const publicPath = path.join(dirname, './public')
+
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("corriendo en el sv 3000")
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(dirname, './views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(dirname, './views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(dirname, './views/login.html'))
+});
+
+
+
+
+
+
+
+
+
+
+
+
